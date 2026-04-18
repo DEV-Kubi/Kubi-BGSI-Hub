23e17856fcb0a39664e4b5505b1303422a7cd493
kubi <jakubpapak7@gmail.com>
2026-04-18 20:36:45 +0200

    fixed executing script

--- ui/mainui.lua
+++ ui/mainui.lua
@@ -12,19 +12,19 @@ do
     _G.ScriptTab = _G.Window:AddTab("Scripting")
 
     if _G.MainTab then
-        loadstring("https://raw.githubusercontent.com/DEV-Kubi/Kubi-BGSI-Hub/refs/heads/main/tabs/main/main.lua?token=GHSAT0AAAAAAD22JRBLLST2XQ5SFBMJQNUC2PDXZHQ")
+        loadstring("https://raw.githubusercontent.com/DEV-Kubi/Kubi-BGSI-Hub/refs/heads/main/tabs/main/main.lua?token=GHSAT0AAAAAAD22JRBLLST2XQ5SFBMJQNUC2PDXZHQ")()
     end
 
     if _G.FarmTab then
-        loadstring("https://raw.githubusercontent.com/DEV-Kubi/Kubi-BGSI-Hub/refs/heads/main/tabs/farming/farming.lua?token=GHSAT0AAAAAAD22JRBLRAS3D6SJVOQVERAI2PDXZPA")
+        loadstring("https://raw.githubusercontent.com/DEV-Kubi/Kubi-BGSI-Hub/refs/heads/main/tabs/farming/farming.lua?token=GHSAT0AAAAAAD22JRBLRAS3D6SJVOQVERAI2PDXZPA")()
     end
 
     if _G.EggsTab then
-        loadstring("https://raw.githubusercontent.com/DEV-Kubi/Kubi-BGSI-Hub/refs/heads/main/tabs/eggs/eggs.lua?token=GHSAT0AAAAAAD22JRBLTRM44QNGZ4UL46BC2PDX2AA")
+        loadstring("https://raw.githubusercontent.com/DEV-Kubi/Kubi-BGSI-Hub/refs/heads/main/tabs/eggs/eggs.lua?token=GHSAT0AAAAAAD22JRBLTRM44QNGZ4UL46BC2PDX2AA")()
     end
 
     if _G.ScriptTab then
-        loadstring("https://raw.githubusercontent.com/DEV-Kubi/Kubi-BGSI-Hub/refs/heads/main/tabs/scripting/scripting.lua?token=GHSAT0AAAAAAD22JRBL63MTAGD5WGMO7TOQ2PDX2PA")
+        loadstring("https://raw.githubusercontent.com/DEV-Kubi/Kubi-BGSI-Hub/refs/heads/main/tabs/scripting/scripting.lua?token=GHSAT0AAAAAAD22JRBL63MTAGD5WGMO7TOQ2PDX2PA")()
     end
 
     _G.MainTab:Show()
