c7f6919e489cd66cfcdc7e5c0d6d5c52741a9442
kubi <jakubpapak7@gmail.com>
2026-04-19 00:21:13 +0200

    commit push

--- tabs/eggs/eggs.lua
+++ tabs/eggs/eggs.lua
@@ -8,17 +8,17 @@ do
     local fastHatch = _G.EggsTab:AddSwitch("Fast Hatch", function(bool)
         turned = false
 		if bool then
-            repeat
-                while task.wait(0.1) do
+            while task.wait(0.1) do
                 VirtualInputManager:SendKeyEvent(true, Enum.KeyCode.R, false, game)
                 task.wait()
                 VirtualInputManager:SendKeyEvent(false, Enum.KeyCode.R, false, game)
+                if turned == true then
+                    break
+                end
             end
-            break
-            until turned == true
         else
             turned = true
-            print("fast hatched turned off")
+            print("fast hatched turned off - test 1")
         end
 	end)
 	fastHatch:Set(false)
