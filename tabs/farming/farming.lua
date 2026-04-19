fd26f7ce22b0e02a8186fdefd436afaafcaf65bb
kubi <jakubpapak7@gmail.com>
2026-04-19 11:46:39 +0200

    up

--- tabs/farming/farming.lua
+++ tabs/farming/farming.lua
@@ -6,20 +6,7 @@ do
     local autoCollectPickups = _G.FarmTab:AddSwitch("Auto Collect Pickups", function(bool)
         turned = false
 		if bool then
-<<<<<<< HEAD
-            print("Start Auto Collecting PPickups")
-=======
-            while task.wait(1) do
-                for i,v in pairs(_G.Tutorial.module_upvr._activePickups) do
-                    v:Destroy()
-                    _G.Tutorial.module_upvr._activePickups[tostring(i)] = nil
-                    game:GetService("ReplicatedStorage").Remotes.Pickups.CollectPickup:FireServer(i)
-                    if turned == true then
-                        break
-                    end
-                end
-            end
->>>>>>> 0dd297562201e91faeb91f902ce7bcc2fd2cdbec
+            print("Starting Auto Collecting Pickups")
         else
             turned = true
         end
